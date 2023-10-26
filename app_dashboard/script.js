197c62caa67eff0cbcb3a7e47b7fea4c3b1c82aa
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-10-26 10:38:23 -0300

    métodos load, get e post

--- app_dashboard/script.js
+++ app_dashboard/script.js
@@ -1,20 +1,25 @@
 $(document).ready(() => {
   $("#documentacao").on("click", () => {
     //$("#pagina").load("documentacao.html");
-    /*$.get("documentacao.html", (data) => {
-      $("#pagina").html(data);
-    });*/
+    //$.get("documentacao.html", (data) => {
+    //podereia ser qualquer parâmetro (está sendo recuperado o conteúdo em si da resposta) - data, valor..
+    //console.log(data);
+    //$("#pagina").html(data);
+    //});
     $.post("documentacao.html", (data) => {
       $("#pagina").html(data);
     });
+    //através da instancia do jQuery ($) será executado o método get (.get()) que espera a url .get('') e uma ação .get('', data => {}) - data pois estamos recuperando o conteúdo em si da resposta
   });
   $("#suporte").on("click", () => {
     //$("#pagina").load("suporte.html");
-  });
-  /*$.get("suporte.html", (data) => {
-    $("#pagina").html(data);
-  });*/
-  $.post("suporte.html", (data) => {
-    $("#pagina").html(data);
+    //$.get("suporte.html", (data) => {
+    //podereia ser qualquer parâmetro (está sendo recuperado o conteúdo em si da resposta) - data, valor..
+    //console.log(data);
+    //$("#pagina").html(data);
+    //});
+    $.post("suporte.html", (data) => {
+      $("#pagina").html(data);
+    });
   });
 });
